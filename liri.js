2ded75e24c8ca76a10a471194bfd6255e6ca91e3
Alan <a-ashbeck@northwestern.edu>
2016-12-14 12:44:24 -0600

    Begin twitter logic

--- liri.js
+++ liri.js
@@ -72,6 +72,13 @@ inquirer.prompt([
 			});
 
 		} else if (user.technology === 'Twitter') {
+			var params = {screen_name: 'nodejs'};
+			
+			client.get('statuses/user_timeline', params, function(error, tweets, response) {
+			  if (!error) {
+			    console.log(tweets);
+			  }
+			});
 
 		} else {
 			// Running the readFile module that's inside of fs.
